85786447b6a046491beb162f7f0a8c84e3d0f4c9
Hemanth Kumar <avisihks@gmail.com>
2023-09-20 21:09:07 +0530

    Add Auth fucntioanlity

--- controllers/ExpenseController.js
+++ controllers/ExpenseController.js
@@ -9,6 +9,38 @@ const createExpense = async (req, res) => {
   }
 };
 
+const getAllExpenses = async (req, res) => {
+  const { user_id } = req.params; // Extract user_id from params
+
+  try {
+    const expenses = await Expense.findAll({
+      where: { user_id }, // Filter expenses by user_id
+    });
+    res.status(200).json(expenses);
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+const deleteExpense = async (req, res) => {
+  const expenseId = req.params.id;
+
+  try {
+    const expense = await Expense.findByPk(expenseId);
+
+    if (!expense) {
+      return res.status(404).json({ message: "Expense not found" });
+    }
+
+    await expense.destroy();
+    res.status(200).json({ message: "Expense deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
 module.exports = {
   createExpense,
+  getAllExpenses,
+  deleteExpense,
 };
--- routes/expenses.js
+++ routes/expenses.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const ExpenseController = require("../controllers/ExpenseController");
 
 router.post("/", ExpenseController.createExpense);
+router.get("/:user_id", ExpenseController.getAllExpenses);
+router.delete("/:id", ExpenseController.deleteExpense);
 
 module.exports = router;
